b62e1be54aa8e4ad7dddee12ca391d2d54e727d0
prostones <wangshilei0927@gmail.com>
2017-07-21 21:30:22 +0800

    XXX

--- get_temp.py
+++ get_temp.py
@@ -4,7 +4,9 @@ import redis
 
 def getTemp():
     r = redis.Redis(host='localhost', port=6379, db=0)
+    print('the first temp is 'r.lindex("temp",0) )
     return r.lindex("temp",0) 
 
 if __name__ == '__main__':
     getTemp()
+    
